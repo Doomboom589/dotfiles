859869456f0c5432e514f89fa2f525d9d9189b25
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-04-07 22:31:20 +0100

    kitty customisation (probably not gonna stick with this i dont like the purple text for everything

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -2,6 +2,12 @@
 {
   programs.kitty = {
     enable = true;
+    font = {
+      name = "Firacode Nerd Font Mono";
+      size = 18;
+    };
+    theme = "Monokai Soda";
     package = pkgs.kitty;
+    shellIntegration.enableFishIntegration = true;
   };
 }
