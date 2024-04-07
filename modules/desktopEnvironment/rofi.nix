93208ea0b53d0aa74298bd8502685e34fd292c5e
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-04-07 15:36:53 +0100

    added new power menu

--- modules/desktopEnvironment/rofi.nix
+++ modules/desktopEnvironment/rofi.nix
@@ -5,4 +5,11 @@
     package = pkgs.rofi.override { plugins = [ pkgs.rofi-emoji ]; };
     theme = ./rofi/theme.rasi;
   };
+  xdg.configFile = {
+    "rofi/powermenu.sh" = {
+      source = ./rofi/powermenu.sh;
+      executable = true;
+    };
+    "rofi/powermenu-theme.rasi".source = ./rofi/powermenu-theme.rasi;
+  };
 }
