60eb89e1f6a4a3f4295c855386ed03b04e280b7d
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-04-07 21:16:08 +0100

    rofi power menu

--- modules/desktopEnvironment/rofi.nix
+++ modules/desktopEnvironment/rofi.nix
@@ -5,11 +5,11 @@
     package = pkgs.rofi.override { plugins = [ pkgs.rofi-emoji ]; };
     theme = ./rofi/theme.rasi;
   };
-  xdg.configFile = {
-    "rofi/powermenu.sh" = {
-      source = ./rofi/powermenu.sh;
+  # hacky solution to local/bin
+  xdg.dataFile = {
+    "../bin/rofi-power-menu" = {
+      source = ./rofi/rofi-power-menu;
       executable = true;
     };
-    "rofi/powermenu-theme.rasi".source = ./rofi/powermenu-theme.rasi;
   };
 }
