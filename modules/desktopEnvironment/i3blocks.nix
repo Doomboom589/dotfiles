d325bfcbfab59b51427f484ecf686926a178daa6
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-04-08 17:47:07 +0100

    added spotify currently playing

--- modules/desktopEnvironment/i3blocks.nix
+++ modules/desktopEnvironment/i3blocks.nix
@@ -3,12 +3,14 @@ let
   bg-1 = "#12121f";
   bg-4 = "#222236";
   fg-1 = "#daddff";
+  fg-5 = "#cccfff";
   grey-1 = "#4f5071";
   red-2 = "#da587d";
   purple-2 = "#8983d8";
   purple-3 = "#7a73d3";
   blue-2 = "#729ce9";
   blue-3 = "#608fe6";
+  green-3 = "#6fd89e";
 in
 {
   programs.i3blocks = {
@@ -16,8 +18,14 @@ in
     package = pkgs.i3blocks;
     bars = {
       top = {
+        "0-spotify" = {
+	  command = "echo \"<span foreground='${green-3}' background='${bg-1}' size='x-large'> </span><span foreground='${bg-4}' background='${green-3}' rise='2pt'><span size='large' > 󰝚 </span>$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 2 \"artist\" | tail -n 1 | grep -o '\".*\"' | sed 's/\"//g' | sed 's/&/&amp;/') - $(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -A 1 \"title\" | tail -n 1 | grep -o '\".*\"' | sed 's/\"//g' | sed 's/&/&amp;/')<span size='large' > 󰝚 </span></span>\"";
+	  markup = "pango";
+	  interval = 1;
+	  separator_block_width = 0;
+	};
 	"1-disk" = {
-	  command = "echo \"<span foreground='${blue-2}' background='${bg-1}' size='x-large'> </span><span background='${bg-1}'><span background='${blue-2}' foreground='${grey-1}' size='large' rise='2pt'>  </span><span background='${blue-2}' foreground='${fg-1}' size='small' rise='2pt'> / </span><span background='${blue-2}' size='small' rise='2pt'>$(df -h | grep \"/$\" | awk '{print $5}') </span></span>\"";
+	  command = "echo \"<span foreground='${blue-2}' background='${green-3}' size='x-large'> </span><span background='${bg-1}'><span background='${blue-2}' foreground='${grey-1}' size='large' rise='2pt'>  </span><span background='${blue-2}' foreground='${fg-1}' size='small' rise='2pt'> / </span><span background='${blue-2}' size='small' rise='2pt'>$(df -h | grep \"/$\" | awk '{print $5}') </span></span>\"";
 	  markup = "pango";
 	  interval = 30;
 	  separator_block_width = 0;
