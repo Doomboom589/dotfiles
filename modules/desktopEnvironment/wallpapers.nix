c58c49190f382391117970c4aef48c966517956c
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-05-06 18:15:53 +0100

    dynamically add wallpapers from wallpapers directory

--- modules/desktopEnvironment/wallpapers.nix
+++ modules/desktopEnvironment/wallpapers.nix
@@ -1,11 +1,12 @@
 { ... }:
 {
-  xdg.configFile = {
-    "wallpapers/brush.jpg".source = ./wallpapers/brush.jpg;
-    "wallpapers/catppuccin_triangle.png".source = ./wallpapers/catppuccin_triangle.png;
-    "wallpapers/nix-black-4k.png".source = ./wallpapers/nix-black-4k.png;
-    "wallpapers/steps.png".source = ./wallpapers/steps.png;
-    "wallpapers/WavesDarkAlt6016x6016.jpg".source = ./wallpapers/WavesDarkAlt6016x6016.jpg;
-    "wallpapers/WavesLight6016x6016.jpg".source = ./wallpapers/WavesLight6016x6016.jpg;
-  };
+   xdg.configFile = builtins.mapAttrs (fileName: { "wallpapers/${fileName}".source = " ./wallpapers/${fileName}"; }) (builtins.readDir ./modules/desktopEnvironment/wallpapers);
+#  xdg.configFile = {
+#    "wallpapers/brush.jpg".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/brush.jpg;
+#    "wallpapers/catppuccin_triangle.png".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/catppuccin_triangle.png;
+#    "wallpapers/nix-black-4k.png".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/nix-black-4k.png;
+#    "wallpapers/steps.png".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/steps.png;
+#    "wallpapers/WavesDarkAlt6016x6016.jpg".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/WavesDarkAlt6016x6016.jpg;
+#    "wallpapers/WavesLight6016x6016.jpg".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/WavesLight6016x6016.jpg;
+#  };
 }
