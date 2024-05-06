16456e352ed1de6a240906fe5151d330b6104c05
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-05-06 18:26:05 +0100

    recursive wallpaper searching

--- modules/desktopEnvironment/wallpapers.nix
+++ modules/desktopEnvironment/wallpapers.nix
@@ -1,12 +1,7 @@
 { ... }:
 {
-   xdg.configFile = builtins.mapAttrs (fileName: { "wallpapers/${fileName}".source = " ./wallpapers/${fileName}"; }) (builtins.readDir ./modules/desktopEnvironment/wallpapers);
-#  xdg.configFile = {
-#    "wallpapers/brush.jpg".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/brush.jpg;
-#    "wallpapers/catppuccin_triangle.png".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/catppuccin_triangle.png;
-#    "wallpapers/nix-black-4k.png".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/nix-black-4k.png;
-#    "wallpapers/steps.png".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/steps.png;
-#    "wallpapers/WavesDarkAlt6016x6016.jpg".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/WavesDarkAlt6016x6016.jpg;
-#    "wallpapers/WavesLight6016x6016.jpg".source = config.lib.file.mkOutOfStoreSymlink ./wallpapers/WavesLight6016x6016.jpg;
-#  };
+  xdg.configFile.wallpapers = {
+    source = ./wallpapers;
+    recursive = true;
+  };
 }
